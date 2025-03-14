ef53e0b8042c34c12e32bc2cf2ddd867e43a970e
DrvppyMar <tdaniel@mail.umw.edu>
2025-03-14 14:25:24 -0400

    Added Reports Page icon on dashboard for admins.

--- index.php
+++ index.php
@@ -122,6 +122,10 @@
                         <i class="fa-solid fa-arrow-up-from-bracket"></i>
                         <span><center>Upload Resources</center></span>
                     </div>
+                    <div class="dashboard-item" data-link="reportsPage.php">
+                        <img src="images/create-report.svg">
+                        <span><center>Reports Page</center></span>
+                    </div>
                 <?php endif ?>
 
                 <!-- FOR VOLUNTEERS AND PARTICIPANTS ONLY -->
